103b0d789495a76272a0f5771ca73b71cdee2bbb
SilentRemix <dshkhamraev@edu.hse.ru>
2021-04-24 13:27:39 +0500

    Initial commit

---
+++ src/main/kotlin/com/mutecomputers/Application.kt
@@ -0,0 +1,36 @@
+package com.mutecomputers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@SpringBootApplication
+class Application
+{
+	@Bean
+	fun docs(): Docket {
+		return Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis(RequestHandlerSelectors.withClassAnnotation(RestController::class.java))
+			.paths(PathSelectors.any())
+			.build()
+			.apiInfo(apiInfo())
+	}
+
+	@Bean
+	fun apiInfo(): ApiInfo {
+		val builder = ApiInfoBuilder()
+		builder.title("Mute Computers").version("1.0.0")
+		return builder.build()
+	}
+}
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/com/mutecomputers/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mutecomputers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
