548fb5477dbccff2304c7ef1cdaa51ab96083af7
unknown <jush1703@gmail.com>
2023-01-13 02:17:55 +0300

    Bug fix and Dockerfile update

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM amazoncorretto:17-alpine
 ADD /build/libs/mute-computers-1.0.0.jar mute-computers.jar
+EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "mute-computers.jar"]
--- src/main/kotlin/com/mutecomputers/controller/MainController.kt
+++ src/main/kotlin/com/mutecomputers/controller/MainController.kt
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("/")
 class MainController @Autowired constructor(
     private val cpuRepository: CPURepository,
     private val gpuRepository: GPURepository,
